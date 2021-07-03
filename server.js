c8c0578ba71669661242d562e63861c4924d81b2
Shane <shane.messmer1@gmail.com>
2021-07-03 18:05:13 -0400

    adds functionality

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { notes } = require('./db/db.json');
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
 
 
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => { 
+  req.body.id = notes.length.toString();
+
+  // add animal to json file and animals array in this function
+  const note = createNewNote(req.body, notes);
+
+  res.json(note);
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 
